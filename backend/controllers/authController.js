4da65abbb31ec650ca3e1455f2c366381bef2619
Sudharsan2k03 <sudharsanramesh747@gmail.com>
2026-03-15 21:52:14 +0530

    Fix bcrypt login comparison

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET || 'secret123', {
@@ -10,9 +11,14 @@ const generateToken = (id) => {
 const authUser = async (req, res) => {
   try {
     const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Please provide email and password' });
+    }
+
     const user = await User.findOne({ email }).populate('facultyId');
 
-    if (user && (await user.matchPassword(password))) {
+    if (user && (await bcrypt.compare(password, user.password))) {
       res.json({
         _id: user._id,
         name: user.name,
@@ -120,7 +126,7 @@ const updateUserPassword = async (req, res) => {
     const user = await User.findById(req.user._id);
     const { currentPassword, newPassword } = req.body;
 
-    if (user && (await user.matchPassword(currentPassword))) {
+    if (user && (await bcrypt.compare(currentPassword, user.password))) {
       user.password = newPassword;
       await user.save();
       res.json({ message: 'Password updated successfully' });
